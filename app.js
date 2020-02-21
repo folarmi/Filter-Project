2147577d4135fc000b9054376fab128d777123da
folarmi <fola4ril@gmail.com>
2020-02-21 13:55:22 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const navBar = document.getElementsByClassName('nav-bar');
+console.log(navBar);
+
+// Toggles Harmburger 
+function myFunction(x) {
+    x.classList.toggle("change");
+
+    // Toggles menu
+    var navBar = document.getElementById("nav-bar");
+    if (navBar.style.display === "block") {
+        navBar.style.display = "none";
+      } else {
+        navBar.style.display = "block";
+      }
+}
